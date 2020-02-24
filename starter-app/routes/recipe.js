8f598acf7f7f40e1c70ef838768fc0a904eb8914
alinepasetti <pasettialine@gmail.com>
2020-02-24 11:47:09 +0000

    search funcionani

--- starter-app/routes/recipe.js
+++ starter-app/routes/recipe.js
@@ -4,11 +4,23 @@ const { Router } = require('express');
 const router = new Router();
 const routeGuard = require('./../middleware/route-guard');
 
+const RECIPE_API_ID = process.env.RECIPE_API_ID;
+const RECIPE_API_KEY = process.env.RECIPE_API_KEY;
+
 const axios = require('axios');
 
 router.get('/search', (req, res, next) => {
-  res.render('index', { title: 'Hello World!' });
+  const { item } = req.query;
+  let recipes;
+  const Recipe = axios.get(
+    `https://api.edamam.com/search?q=${item}&app_id=${RECIPE_API_ID}&app_key=${RECIPE_API_KEY}`
+  );
+  Recipe.then(output => {
+    recipes = output.data.hits;
+    res.render('recipe/search', { recipes });
+  }).catch(error => {
+    next(error);
+  });
 });
-
 
 module.exports = router;
