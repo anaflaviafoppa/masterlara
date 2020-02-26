3514823eb2520d53e385b15692b2ea05bb026c37
Ana Flavia Foppa <anaffoppa@gmail.com>
2020-02-26 17:42:20 +0000

    single

--- starter-app/public/scripts/script.js
+++ starter-app/public/scripts/script.js
@@ -1,3 +1,15 @@
 window.addEventListener('load', () => {
   console.log('Ironmaker app started successfully!');
 }, false);
+
+
+function modifyImage() {
+  var img1 = "https://res.cloudinary.com/ddz8awd1y/image/upload/v1582735089/LIke/like_bzch3o.png";
+  var img2 = "https://res.cloudinary.com/ddz8awd1y/image/upload/v1582735090/LIke/dislike_v9or0v.png";
+
+  var imgElement = (document.getElementById("imageLike"));
+
+  imgElement.src = (imgElement.src === img1)? img2 :img1;
+  
+}
+
