557563c6b9d70516e9ce6844d8568bc902bf8676
Ana Flavia Foppa <anaffoppa@gmail.com>
2020-02-28 13:02:30 +0000

    welcome msg done

--- passport-configuration.js
+++ passport-configuration.js
@@ -70,7 +70,7 @@ passport.use(
             to: user.email,
             subject:'Welcome to Master Lara!',
             //text: 'Hello World!'
-            html:"<img style='width:200px; ' src='https://res.cloudinary.com/ddz8awd1y/image/upload/v1582219129/profileImage/fykbpstjwd2clqjbwbyl.jpg'>"
+            html:"<a url='https://master-lara.herokuapp.com/' ><img  src='https://res.cloudinary.com/ddz8awd1y/image/upload/v1582894691/welcome%20email/welcome_poocbk.png'></a>"
           });
         })
         .then(() => {
